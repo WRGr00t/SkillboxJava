d352b59fbc787d48d2847f451e2bb2582d117dd1
WRGr00t <val.kotelnikov@mail.ru>
2019-09-29 00:57:21 +0900

    Правки по рекоммендациям преподавателя

--- Unit9/3/htmlParse/src/Loader.java
+++ Unit9/3/htmlParse/src/Loader.java
@@ -2,22 +2,22 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.*;
 
 public class Loader {
-    private static Path target = Paths.get("src\\folderForImg");
+    private static Path target = Paths.get("src", "folderForImg");
 
     public static void main(String[] args) throws IOException {
         Document doc;
         Elements links = new Elements();
         try {
             doc = Jsoup.connect("https://lenta.ru/").get();
+            Jsoup.connect("https://lenta.ru/").maxBodySize(0);
             links = doc.select("img");
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
         for (Element category : links) {
             download(category.select("img").attr("abs:src"),target.toString());
@@ -28,10 +28,13 @@ public class Loader {
         try {
             URL url = new URL(sourceURL);
             String fileName = sourceURL.substring(sourceURL.lastIndexOf('/') + 1);
-            File target = new File(targetDirectory + File.separator + fileName);
-            Path targetPath = target.toPath();
+            Path targetPath = Paths.get(targetDirectory, fileName);
             System.out.println("Скачан файл " + targetPath.getFileName());
-            Files.copy(url.openStream(), targetPath, StandardCopyOption.REPLACE_EXISTING);
+            try {
+                Files.copy(url.openStream(), targetPath, StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
         } catch (Exception ex){
             System.out.println(ex.getMessage());
         }
