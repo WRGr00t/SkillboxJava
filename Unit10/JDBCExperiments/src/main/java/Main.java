4bac1a5b64401d17397db1a83e8e4d4eb62e9af9
WRGr00t <val.kotelnikov@mail.ru>
2019-11-04 23:02:11 +0900

    Стартовый коммит по SQL

--- Unit10/JDBCExperiments/src/main/java/Main.java
+++ Unit10/JDBCExperiments/src/main/java/Main.java
@@ -2,7 +2,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,29 +9,22 @@ public class Main {
                 "useLegacyDatetimeCode=false&serverTimezone=UTC";
         String user = "root";
         String password = "Va12821393___";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MMM/dd HH:mm:ss");
-
-        try{
+        try {
             Connection connection = DriverManager.getConnection(url, user, password);
-
             Statement statement = connection.createStatement();
-            //"SELECT course_name, subscription_date FROM purchaselist GROUP BY course_name"
             ResultSet resultSet = statement.executeQuery("SELECT course_name, COUNT(*), MONTH(subscription_date), " +
                     "YEAR(subscription_date), COUNT(MONTH(subscription_date)) FROM purchaselist " +
                     "GROUP BY YEAR(subscription_date), MONTH(subscription_date), course_name");
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 String nameCourses = resultSet.getString("course_name");
-                //String dateSubscription = resultSet.getString("subscription_date");
-                //LocalDate dateSubscription = LocalDate.parse(resultSet.getString("subscription_date"), formatter);
                 String count = resultSet.getString("COUNT(MONTH(subscription_date))");
                 System.out.print(nameCourses + "\t");
-                //System.out.println(dateSubscription);
                 System.out.println(count);
             }
             resultSet.close();
             statement.close();
             connection.close();
-        } catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
