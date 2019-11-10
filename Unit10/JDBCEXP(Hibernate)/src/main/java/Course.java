a4da80dac962e2dfcae5c34eab4d56e98604c0c8
WRGr00t <val.kotelnikov@mail.ru>
2019-11-11 00:16:40 +0900

    Связи в базе (стихи)

--- Unit10/JDBCEXP(Hibernate)/src/main/java/Course.java
+++ Unit10/JDBCEXP(Hibernate)/src/main/java/Course.java
@@ -1,4 +1,5 @@
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "Courses")
@@ -13,13 +14,19 @@ public class Course {
     @Column(columnDefinition = "enum")
     private CourseType type;
     private String description;
-    @Column(name = "teacher_id")
-    private int teacherId;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Teacher teacher;
     @Column(name = "students_count")
     private int studentsCount;
     private int price;
     @Column(name = "price_per_hour")
     private float pricePerHour;
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "Subscriptions",
+        joinColumns = {@JoinColumn(name = "course_id")},
+        inverseJoinColumns = {@JoinColumn(name = "student_id")})
+
+    private List<Student> students;
 
     public int getId() {
         return id;
@@ -61,12 +68,12 @@ public class Course {
         this.description = description;
     }
 
-    public int getTeacherId() {
-        return teacherId;
+    public Teacher getTeacher() {
+        return teacher;
     }
 
-    public void setTeacherId(int teacherId) {
-        this.teacherId = teacherId;
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
     }
 
     public int getStudentsCount() {
@@ -91,5 +98,13 @@ public class Course {
 
     public void setPricePerHour(float pricePerHour) {
         this.pricePerHour = pricePerHour;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
     }
 }
--- Unit10/JDBCEXP(Hibernate)/src/main/java/Main.java
+++ Unit10/JDBCEXP(Hibernate)/src/main/java/Main.java
@@ -1,9 +1,12 @@
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,9 +16,13 @@ public class Main {
         Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
         SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
         Session session = sessionFactory.openSession();
-        Course course = session.get(Course.class, 12);
-        System.out.println(course.getName() + " имеет тип - " + course.getType());
-
+        Transaction transaction = session.beginTransaction();
+        Course course = session.get(Course.class, 1);
+        List<Student> students = course.getStudents();
+        for (Student student : students){
+            System.out.println(student.getName());
+        }
+        transaction.commit();
         sessionFactory.close();
     }
 }
