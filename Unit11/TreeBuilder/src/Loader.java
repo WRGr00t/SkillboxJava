7c17311d6a83b78241624b05f581d6ca26d0682f
WRGr00t <val.kotelnikov@mail.ru>
2020-02-28 01:24:51 +0900

    домашние попытки

--- Unit11/TreeBuilder/src/Loader.java
+++ Unit11/TreeBuilder/src/Loader.java
@@ -7,10 +7,18 @@ public class Loader {
     public static void main(String[] args) throws MalformedURLException {
         String path = "https://skillbox.ru/";
         Link rootLink = new Link(path, Link.getSublink(path), 0);
-        //new ForkJoinPool().invoke(new TreeBuilder(path));
-        ///ConcurrentSkipListSet<String> links = rootLink.getSublink(path);
+        TreeBuilder treeBuilder = new TreeBuilder(rootLink);
+        ConcurrentSkipListSet<String> links = new ForkJoinPool().invoke(treeBuilder);
+        /*for (String string : links){
+            ConcurrentSkipListSet<String> subLinks = new ForkJoinPool().invoke(new TreeBuilder(new Link(string, Link.getSublink(string), 1)));
+            for (String s : subLinks){
+                links.add(s);
+            }
+        }*/
+        //ConcurrentSkipListSet<String> links = rootLink.getSublink(path);
         //TreeSet<String> resultSet = new TreeSet<>();
-        System.out.println(rootLink.buildMap());
+        treeBuilder.buildBuild(rootLink, 0);
+        System.out.println(links.size());
     }
 
 }
--- Unit11/TreeBuilder/src/TreeBuilder.java
+++ Unit11/TreeBuilder/src/TreeBuilder.java
@@ -1,23 +1,26 @@
+import java.net.MalformedURLException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.RecursiveTask;
 
-public class TreeBuilder extends RecursiveTask<ConcurrentSkipListSet<String>> {
-    private final String link;
+public class TreeBuilder extends RecursiveTask<ConcurrentSkipListSet<String>> implements Comparable<Link>{
+    private final Link link;
 
-    public TreeBuilder(String link) {
+    public TreeBuilder(Link link) {
         this.link = link;
     }
 
     @Override
     protected ConcurrentSkipListSet<String> compute() {
-        ConcurrentSkipListSet subLinks = Link.getSublink(link);
+        ConcurrentSkipListSet subLinks = link.getSubLinks();
         List<TreeBuilder> subTasks = new LinkedList<>();
 
-        for (String sublink : Link.getSublink(link)){
-            TreeBuilder task = new TreeBuilder(sublink);
+        for (String sublink : link.getSubLinks()){
+            System.out.println(sublink);
+            TreeBuilder task = new TreeBuilder(new Link(sublink, Link.getSublink(sublink), link.getLevel() + 1));
             task.fork();
+            System.out.println(Thread.currentThread().getName());
             subLinks.add(task);
         }
 
@@ -26,4 +29,28 @@ public class TreeBuilder extends RecursiveTask<ConcurrentSkipListSet<String>> {
         }
         return subLinks;
     }
+
+    @Override
+    public int compareTo(Link l) {
+        return link.getLink().compareTo(l.getLink());
+    }
+
+    public String buildBuild(Link link, int level) throws MalformedURLException {
+        System.out.println("build...");
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("\t".repeat(level))
+                .append(this)
+                .append("\n");
+        for (String url : link.getSubLinks()){
+            System.out.println(url);
+            stringBuffer.append(new Link(url, Link.getSublink(url), level + 1).buildMap());
+        }
+        return stringBuffer.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "\t".repeat(link.getLevel()) + link.toString();
+    }
+
 }
