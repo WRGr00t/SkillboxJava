dadf96873df9ff1d1ab3cb9034909c4a73372605
Kotelnikov Valentin <val.kotelnikov@mail.ru>
2020-02-27 13:31:15 +0900

    удалил лишнее

--- getSiteMap/src/Loader.java
+++
@@ -1,8 +0,0 @@
-import java.net.MalformedURLException;
-
-public class Loader {
-    public static void main(String[] args) throws MalformedURLException {
-        Link link = new Link("https://lenta.ru/");
-        System.out.println(link.buildMap());
-    }
-}
