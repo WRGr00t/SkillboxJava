73f1b93cbfb420ada62b9352fe7f025bdf970b34
WRGr00t <val.kotelnikov@mail.ru>
2020-03-06 00:40:48 +0900

    Домашние потуги

---
+++ Unit11/SiteTreeFromParallelStream/src/Main.java
@@ -0,0 +1,14 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "https://skillbox.ru/";
+        Path pathForWriteFile = Paths.get("src/map.txt");
+        ConcurrentSkipListSet<String> uniqueURL = new ConcurrentSkipListSet<>();
+        ReaderLinks reader = new ReaderLinks(uniqueURL, url);
+        reader.get_links(url);
+        ReaderLinks.saveToFile(pathForWriteFile, ReaderLinks.getUniqueURL());
+    }
+}
--- Unit11/SiteTreeFromParallelStream/src/ReaderLinks.java
+++ Unit11/SiteTreeFromParallelStream/src/ReaderLinks.java
@@ -2,25 +2,22 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashSet;
+import java.nio.file.Path;
 import java.util.Set;
-import java.util.TreeSet;
 import java.util.concurrent.ConcurrentSkipListSet;
 
-public class readAllLinks {
+public class ReaderLinks {
+    public static ConcurrentSkipListSet<String> uniqueURL;
+    public static String site;
 
-    public static ConcurrentSkipListSet<String> uniqueURL = new ConcurrentSkipListSet<>();
-    public static String my_site;
-
-    public static void main(String[] args) {
-
-        readAllLinks obj = new readAllLinks();
-        my_site = "https://skillbox.ru/";
-        obj.get_links(my_site);
+    public ReaderLinks(ConcurrentSkipListSet uniqueURL, String site) {
+        this.uniqueURL = uniqueURL;
+        this.site = site;
     }
 
-    private void get_links(String url) {
+    public void get_links(String url) {
         try {
             Document doc = Jsoup.connect(url).userAgent("Mozilla").get();
             Elements links = doc.select("a");
@@ -33,7 +30,7 @@ public class readAllLinks {
                     .unordered()
                     .map((link) -> link.absUrl("href")).forEach((this_url) -> {
                 boolean add = uniqueURL.add(this_url);
-                if (add && this_url.contains(my_site)) {
+                if (add && this_url.contains(site)) {
                     System.out.println(this_url + " " + Thread.currentThread().getName() + " size = " + uniqueURL.size());
                     try {
                         Thread.sleep(1000);
@@ -45,6 +42,24 @@ public class readAllLinks {
             });
 
         } catch (IOException ex) {
+        }
+    }
+
+    public static ConcurrentSkipListSet<String> getUniqueURL() {
+        return uniqueURL;
+    }
+
+    public static void saveToFile(Path path, Set<String> strings){
+        try {
+            FileWriter file = new FileWriter(path.toString());
+            for (String string : strings){
+                file.write(string + "\n");
+            }
+            file.flush();
+            file.close();
+            System.out.println("File " + path + " saved successfully");
+        } catch (IOException e) {
+            System.err.println("Some problems with file " + e.getMessage());
         }
     }
 }
